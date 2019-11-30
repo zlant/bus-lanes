8944485350a5511002795db578b02b23f667a837
Noémie <noemie.lehuby@zaclys.net>
2019-11-30 20:34:17 +0100

    remove dead code

--- config.js
+++ config.js
@@ -1,20 +1,6 @@
 var editorName = 'BusLanes'
 var version = '0.2.0'
 
-var valuesLane = ['parallel', 'diagonal', 'perpendicular', 'no_parking', 'no_stopping', 'marked', 'fire_lane'];
-var valuesCond = ['free', 'ticket', 'disc', 'residents', 'customers', 'private'];
-
-var legend = [
-    { condition: 'disc', color: 'yellowgreen', text: 'Disc' },
-    { condition: 'no_parking', color: 'orange', text: 'No parking' },
-    { condition: 'no_stopping', color: 'salmon', text: 'No stopping' },
-    { condition: 'free', color: 'limegreen', text: 'Free parking' },
-    { condition: 'ticket', color: 'dodgerblue', text: 'Paid parking' },
-    { condition: 'customers', color: 'greenyellow', text: 'For customers' },
-    { condition: 'residents', color: 'hotpink', text: 'For residents' },
-    { condition: 'disabled', color: 'turquoise', text: 'Disabled' }
-];
-
 var useTestServer = false;
 
 var urlOverpass = 'https://overpass-api.de/api/interpreter?data=';
